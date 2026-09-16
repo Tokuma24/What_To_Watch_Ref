22938c2d8a658b79c439c749b8008ae912519613
Dmitry Suslov <PrototypetheDarkauron@gmail.com>
2026-09-16 19:50:19 +0500

    Initial commit

--- settings.py
+++ settings.py
@@ -1,12 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-# ЭТА СТРОКА КРИТИЧЕСКИ ВАЖНА: она читает файл .env
-load_dotenv()
 
 
-class Config:
-    # Берем значение из .env
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
